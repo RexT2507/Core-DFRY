a3bc75eb0cfd7012e8cffb6d08a3eca5269ea4c5
Doryan LOPEZ <lopezdoryan@gmail.com>
2020-06-19 11:33:30 +0200

    [ADD] Structure générique (ApiLibrary avc Base, Attributs & Extensions) + Cocktails - Modèle & DbContext

---
+++ src/Core/ApiLibrary/Core/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using ApiLibrary.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiLibrary.Base
+{
+    public class BaseEntity
+    {
+        [Omitted]
+        public DateTime createdAt { get; set; }
+        [Omitted]
+        public DateTime? updatedAt { get; set; }
+        [Omitted]
+        public DateTime? deletedAt { get; set; }
+
+
+    }
+}
