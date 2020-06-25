32157b936a94052e8dc68d264f83fc9bf52d8362
Dams <deams.work@gmail.com>
2020-06-25 11:40:56 +0200

    [DEV] Ajout de la partie Order

---
+++ src/Services/Orders/Orders.API/Controllers/OrderDetailsController.cs
@@ -0,0 +1,19 @@
+﻿using ApiLibrary.Core.Base;
+using ApiLibrary.Core.Controllers;
+using Orders.API.Data;
+using Orders.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orders.API.Controllers
+{
+    public class OrderDetailsController : ControllerRef<OrdersDbContext, OrderDetails, int>
+    {
+        public OrderDetailsController(OrdersDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ src/Services/Orders/Orders.API/Data/OrdersDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ApiLibrary.Core.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Orders.API.Models;
+
+namespace Orders.API.Data
+{
+    public class OrdersDbContext : BaseDbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrdersDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
