214fb303dcf4a090dac6e2a52887dd00952d8576
Doryan LOPEZ <lopezdoryan@gmail.com>
2020-06-25 20:48:22 +0200

    [ADD] Script Powershell pour la création des images + Dockerfile pour Auth et Order

---
+++ docker_image_creation.ps1
@@ -0,0 +1,17 @@
+﻿Copy-Item "src/Services/Auth/Auth.API/Dockerfile" -Destination "Dockerfile"
+docker image build -t k8s_auth .
+Remove-Item "Dockerfile"
+
+Copy-Item "src/Services/Cocktails/Cocktails.API/Dockerfile" -Destination "Dockerfile"
+docker image build -t k8s_cocktail .
+Remove-Item "Dockerfile"
+
+Copy-Item "src/Services/Orders/Orders.API/Dockerfile" -Destination "Dockerfile"
+docker image build -t k8s_order .
+Remove-Item "Dockerfile"
+
+docker run -p 8000:8080 --detach --name k8s_auth_container k8s_auth
+
+docker run -p 4002:8080 --detach --name k8s_cocktail_container k8s_cocktail
+
+docker run -p 3500:8080 --detach --name k8s_order_container k8s_order
